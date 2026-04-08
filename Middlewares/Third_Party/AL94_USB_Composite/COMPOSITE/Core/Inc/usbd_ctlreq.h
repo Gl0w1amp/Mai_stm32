e8bd390212344702babb8c70afbc2a568b79c202
グローランプ <130208311+Gl0w1amp@users.noreply.github.com>
2026-04-08 18:40:56 +1000

    feat: add usb container id descriptors

--- Middlewares/Third_Party/AL94_USB_Composite/COMPOSITE/Core/Inc/usbd_ctlreq.h
+++ Middlewares/Third_Party/AL94_USB_Composite/COMPOSITE/Core/Inc/usbd_ctlreq.h
@@ -80,6 +80,7 @@ USBD_StatusTypeDef USBD_StdEPReq(USBD_HandleTypeDef *pdev, USBD_SetupReqTypedef 
 void USBD_CtlError(USBD_HandleTypeDef *pdev, USBD_SetupReqTypedef *req);
 void USBD_ParseSetupRequest(USBD_SetupReqTypedef *req, uint8_t *pdata);
 void USBD_GetString(uint8_t *desc, uint8_t *unicode, uint16_t *len);
+uint8_t *USBD_GetMicrosoftOSStrDescriptor(USBD_SpeedTypeDef speed, uint16_t *length);
 
 /**
   * @}
